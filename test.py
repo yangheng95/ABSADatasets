d5d1baf8c0b7621cac7c8f4a89e38a252e1e8746
yangheng95 <yangheng@m.scnu.edu.cn>
2022-06-23 13:35:10 +0100

    rm_train_inference_sets

--- test.py
+++ test.py
@@ -4,7 +4,10 @@
 # author: yangheng <yangheng@m.scnu.edu.cn>
 # github: https://github.com/yangheng95
 # Copyright (C) 2021. All Rights Reserved.
+import findfile
+from findfile import rm_files
 from pyabsa.utils.file_utils import generate_inference_set_for_apc, convert_apc_set_to_atepc_set
 
-generate_inference_set_for_apc('121')
-convert_apc_set_to_atepc_set('121')
+# generate_inference_set_for_apc('121')
+# convert_apc_set_to_atepc_set('121')
+rm_files(key=['train','inference'])

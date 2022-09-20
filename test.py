99accdc5beb379d89dc5b3e7fa97b11dd0289d7a
yangheng <hy345@exeter.ac.uk>
2022-09-20 15:45:42 +0100

    2022.09.20

--- test.py
+++ test.py
@@ -10,14 +10,33 @@ import findfile
 from findfile import rm_files
 from pyabsa.utils.file_utils import generate_inference_set_for_apc, convert_apc_set_to_atepc_set
 
-findfile.rm_files(os.getcwd(),'.ignore.atepc')
-findfile.rm_files(os.getcwd(),'.atepc.ignore')
+# from pyabsa import APCCheckpointManager
+# classifier = APCCheckpointManager.get_sentiment_classifier('fast')
+# label_map = {0: 'Negative', 1: 'Neutral', 2: 'Positive', '0': 'Negative', '1': 'Neutral', '2': 'Positive'}
+# def read_csv_by_pandas(path):
+#     import pandas as pd
+#     df = pd.read_csv(path)
+#     with open(path+'.dat', 'w', encoding='utf-8') as f:
+#         for index, row in df.iterrows():
+#             if row['aspect'].strip() and row['text'].strip() and row['aspect'] in row['text']:
+#                 text = row['text'].replace('\r', '').replace('\n', '').strip()
+#                 aspect = row['aspect'].replace('\r', '').replace('\n', '').strip()
+#                 res = classifier.infer(text.replace(aspect, '[ASP]{}[ASP]'.format(aspect)))
+#                 f.write(text.replace(aspect, '$T$')+'\n')
+#                 f.write(aspect+'\n')
+#                 f.write(label_map[res['sentiment'][0]]+'\n')
+#                 # f.write(label_map[row['label']]+'\n')
+#     return df
+# read_csv_by_pandas('datasets/apc_datasets/122.Kaggle/test.csv')
+
+
+
+findfile.rm_files(os.getcwd(), '.ignore.atepc')
+findfile.rm_files(os.getcwd(), '.atepc.ignore')
 
 # batch conversion for all ABSA datasets
 generate_inference_set_for_apc('datasets')
 convert_apc_set_to_atepc_set('datasets')
-
-
 
 # default set ATEPC augmented datasets invisible
 for f in findfile.find_cwd_files('.ignore.atepc'):
